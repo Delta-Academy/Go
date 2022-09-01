c4abf805475bb1e30920ee626542c2dafdc9d20a
neuromantic99 <jrowlandm@gmail.com>
2022-09-01 11:31:33 +0100

    game working fully with visuals and reward etc.  Added env wrapper to game_mechanics that was previously mistakenly being git ignored.

--- delta_Go/env_wrapper.py
+++ delta_Go/env_wrapper.py
@@ -1,5 +1,3 @@
-from typing import Callable
-
 import random
 import time
 from typing import Callable, Dict, Optional
@@ -8,59 +6,6 @@ import numpy as np
 from gym.spaces import Box, Discrete
 
 from pettingzoo.utils import BaseWrapper
-
-import numpy as np
-
-from pettingzoo.classic import go_v5
-
-BOARD_SIZE = 7
-
-ALL_POSSIBLE_MOVES = np.arange(BOARD_SIZE**2 + 1)
-
-
-def GoEnv(
-    opponent_choose_move: Callable[[np.ndarray, np.ndarray], int],
-    verbose: bool = False,
-    render: bool = False,
-    game_speed_multiplier: int = 0,
-):
-    return DeltaEnv(
-        go_v5.env(board_size=BOARD_SIZE, komi=0),
-        opponent_choose_move,
-        verbose,
-        render,
-        game_speed_multiplier=game_speed_multiplier,
-    )
-
-
-def choose_move_randomly(observation, legal_moves):
-    return np.random.choice(legal_moves)
-
-
-def choose_move_pass(observation, legal_moves) -> int:
-    "passes on every turn"
-    return BOARD_SIZE**2
-
-
-def play_go(
-    your_choose_move: Callable,
-    opponent_choose_move: Callable,
-    game_speed_multiplier=1,
-    render=True,
-    verbose=False,
-) -> None:
-
-    env = GoEnv(
-        opponent_choose_move,
-        verbose=verbose,
-        render=render,
-        game_speed_multiplier=game_speed_multiplier,
-    )
-
-    observation, reward, done, info = env.reset()
-    while not done:
-        action = your_choose_move(observation, info["legal_moves"])
-        observation, reward, done, info = env.step(action)
 
 
 class DeltaEnv(BaseWrapper):
--- delta_Go/jimmy_main.py
+++ delta_Go/jimmy_main.py
@@ -1,4 +1,6 @@
+import time
 from typing import Any, Dict
+import matplotlib.pyplot as plt
 
 from tqdm import tqdm
 
@@ -9,25 +11,112 @@ TEAM_NAME = "Team Jimmy"  # <---- Enter your team name here!
 assert TEAM_NAME != "Team Name", "Please change your TEAM_NAME!"
 
 
-def train() -> Dict:
-    """
-    TODO: Write this function to train your algorithm.
+# class ChooseMoveCheckpoint:
+#     def __init__(self, checkpoint_name: str):
+#         self.neural_network = copy.deepcopy(load_checkpoint(checkpoint_name))
 
-    Returns:
-    """
-    raise NotImplementedError("You need to implement this function!")
+#     def choose_move(self, state, legal_moves):
+#         neural_network = self.neural_network
+#         mask = np.isin(np.arange(4), legal_moves)
+#         action, _ = neural_network.predict(state, deterministic=False, action_masks=mask)
+#         return action
 
 
-def choose_move(state: Any, user_file: Any, verbose: bool = False) -> int:
-    """Called during competitive play. It acts greedily given current state of the board and value
-    function dictionary. It returns a single move to play.
+# class CustomCallback(BaseCallback):
+#     def __init__(self, verbose=0):
+#         super(CustomCallback, self).__init__(verbose)
+#         self.rewards = []
 
-    Args:
-        state:
+#     def _on_step(self) -> None:
+#         """Called every step()"""
+#         self.rewards.append(safe_mean([ep_info["r"] for ep_info in self.model.ep_info_buffer]))
 
-    Returns:
-    """
-    raise NotImplementedError("You need to implement this function!")
+
+# def mask_fn(env):
+#     return env.last()[0]["action_mask"]
+
+
+# def smooth_trace(trace: np.ndarray, one_sided_window_size: int = 3) -> np.ndarray:
+#     """Smooths a trace by averaging over a window of size one_sided_window_size."""
+#     window_size = int(2 * one_sided_window_size + 1)
+#     trace[one_sided_window_size:-one_sided_window_size] = (
+#         np.convolve(trace, np.ones(window_size), mode="valid") / window_size
+#     )
+#     return trace
+
+
+# def train() -> Dict:
+
+#     #############
+#     # Play against hard-coded opponent
+
+#     # env = PokerEnv(choose_move_rules, verbose=True, render=False)
+#     # env = ActionMasker(env, mask_fn)
+#     # env.reset()
+
+#     # model = MaskablePPO(MaskableActorCriticPolicy, env, verbose=2, ent_coef=0.01)
+
+#     # env.reset()
+
+#     # # model = PPO.load("checkpoint1")
+#     # # model.set_env(env)
+
+#     # callback = CustomCallback()
+#     # model.learn(total_timesteps=250_000, callback=callback)
+#     # model.save("checkpoint1")
+#     # print("reached checkpoint1 \n\n\n\n\n")
+#     # plt.plot(smooth_trace(callback.rewards, 100))
+#     # plt.draw()
+#     # time.sleep(20)
+#     # plt.clf()
+
+#     # test_model(model)
+
+#     ################
+#     # Play checkpointed self
+
+#     choose_move_checkpoint = ChooseMoveCheckpoint("round6").choose_move
+
+#     env = PokerEnv(choose_move_checkpoint, verbose=True, render=False)
+#     env = ActionMasker(env, mask_fn)
+#     env.reset()
+
+#     # model = MaskablePPO(MaskableActorCriticPolicy, env, verbose=2, ent_coef=0.01)
+#     model = MaskablePPO.load("round6")
+#     model.set_env(env)
+
+#     callback = CustomCallback()
+#     model.learn(total_timesteps=500_000, callback=callback)
+
+#     model.save("round7")
+#     print("reached checkpoint \n\n\n\n\n")
+#     plt.plot(smooth_trace(callback.rewards, 2048))
+#     plt.title(f"mean: {np.nanmean(callback.rewards)}, std: {np.nanstd(callback.rewards)}")
+#     plt.axhline(0)
+
+#     plt.show()
+#     return model
+
+
+# def train() -> Dict:
+#     """
+#     TODO: Write this function to train your algorithm.
+
+#     Returns:
+#     """
+#     raise NotImplementedError("You need to implement this function!")
+
+
+# def choose_move(state: Any, user_file: Any, verbose: bool = False) -> int:
+#     """Called during competitive play. It acts greedily given current state of the board and value
+#     function dictionary. It returns a single move to play.
+
+#     Args:
+#         state:
+
+#     Returns:
+#     """
+#     raise NotImplementedError("You need to implement this function!")
 
 
 def n_games():
@@ -36,10 +125,11 @@ def n_games():
         play_go(
             your_choose_move=choose_move_randomly,
             opponent_choose_move=choose_move_randomly,
-            game_speed_multiplier=0.5,
+            game_speed_multiplier=0.1,
             render=True,
             verbose=True,
         )
+        time.sleep(1000)
 
 
 if __name__ == "__main__":
