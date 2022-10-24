c8c0f14741c2890ee8550c4f95748bdc270c4e4f
neuromantic99 <jrowlandm@gmail.com>
2022-10-24 14:52:39 +0100

    think got transition function working without env copying

--- tests/test_transition_function.py
+++ tests/test_transition_function.py
@@ -6,7 +6,14 @@ from pathlib import Path
 import numpy as np
 
 import pytest
-from delta_Go.game_mechanics import GoEnv, choose_move_randomly, transition_function
+from delta_Go.game_mechanics import (
+    BOARD_SIZE,
+    GoEnv,
+    choose_move_pass,
+    choose_move_randomly,
+    transition_function,
+)
+from pettingzoo.classic.go.go_base import Position
 from tqdm import tqdm
 
 HERE = Path(__file__).parent.parent.resolve()
@@ -15,20 +22,48 @@ sys.path.append(str(HERE))
 sys.path.append(str(HERE / "delta_Go"))
 
 
-def test_transition_function():
-    env = GoEnv(choose_move_randomly)
+def test_transition_function_takes_move():
+    env = GoEnv(choose_move_pass)
+    env.reset()
+    state = env.env.go_game
+
+    new_state = transition_function(state, action=0)
+    assert not np.array_equal(state.board, new_state.board)
+
+
+def choose_move_not_top_left(legal_moves, **kwargs):
+    legal_moves = legal_moves[legal_moves != 0]
+
+    if len(legal_moves) > 1:  # Don't pass if you don't have to
+        legal_moves = legal_moves[legal_moves != BOARD_SIZE**2]
+
+    return random.choice(legal_moves)
+
+
+def test_transition_function_correct_move():
+
+    env = GoEnv(choose_move_pass)  # Will leave a new game board after reset
     env.reset()
 
-    transition_env = transition_function(
-        env,
-        # Random move without passing
-        action=choose_move_randomly(env.observation, env.legal_moves[:-1]),
-    )
+    starting_state: Position = env.env.go_game
+    starting_board = starting_state.board
+    assert np.all(starting_board == 0)
 
-    assert not np.array_equal(env.observation, transition_env.observation)
+    player_int = -1 if env.player == "white_0" else 1
+
+    # Make the same move on the transition function and the env
+    # Should give the same result
+    new_state = transition_function(starting_state, action=0)
+    new_board = new_state.board
+    env.step(move=0)
+
+    # Check the state hasn't changed in place
+    assert np.all(starting_state.board == 0)
+
+    assert new_board[0, 0] == player_int
+
+    np.testing.assert_array_equal(env.env.go_game.board, new_board)
 
 
-def test_test_transition_function():
-
-    for _ in tqdm(range(100)):
-        test_transition_function()
+# if __name__ == "main":
+#     test_transition_function_correct_move()
