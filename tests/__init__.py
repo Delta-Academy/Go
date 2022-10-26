b8baeb0eb5fcdbdddec580ccbd132a6fd921c2d5
Henry Pulver <henrypulver@hotmail.co.uk>
2022-10-26 16:31:49 +0100

    Change delta_Go to delta_go in tests __init__

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,4 +3,4 @@ from pathlib import Path
 
 HERE = Path(__file__).parent.parent.resolve()  # isort: ignore
 sys.path.append(str(HERE))  # isort: ignore
-sys.path.append(str(HERE / "delta_Go"))  # isort: ignore
+sys.path.append(str(HERE / "delta_go"))  # isort: ignore
