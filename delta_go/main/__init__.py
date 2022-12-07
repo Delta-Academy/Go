ffaccf23b0ec87daf17e3f26b8de445685b011c5
neuromantic99 <jrowlandm@gmail.com>
2022-12-07 15:11:37 +0100

    remove __init__.py

--- delta_go/main/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-from pathlib import Path
-
-from .main import MCTS, TEAM_NAME, choose_move
-
-HERE = Path(__file__).parent.parent.parent.resolve()  # isort: ignore
-sys.path.append(str(HERE))  # isort: ignore
-sys.path.append(str(HERE / "delta_go"))  # isort: ignore
