618170b60c4688de4cc4b393f1b5025de79cd5a1
neuromantic99 <jrowlandm@gmail.com>
2022-12-07 15:13:18 +0100

    remove path stuff

--- delta_go/game_mechanics/__init__.py
+++ delta_go/game_mechanics/__init__.py
@@ -17,7 +17,3 @@ from .go_env import (
 )
 from .state import State
 from .utils import BLACK, BOARD_SIZE, EMPTY, MAX_NUM_MOVES, PASS_MOVE, WHITE
-
-HERE = Path(__file__).parent.parent.parent.resolve()  # isort: ignore
-sys.path.append(str(HERE))  # isort: ignore
-sys.path.append(str(HERE / "delta_go"))  # isort: ignore
